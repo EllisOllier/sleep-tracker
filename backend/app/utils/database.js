2a72f1180e9b6a6ee7254a71b248e316fe0f1269
Megan L <isaac.rideal@gmail.com>
2026-02-14 16:04:18 +0000

    Connected to database

--- backend/app/utils/database.js
+++ backend/app/utils/database.js
@@ -1,4 +1,6 @@
-const better_sqlite3 = require(`better-sqlite3`).verbose();
-const path = require(`path`)
+const better_sqlite3 = require(`better-sqlite3`);
+const path = require(`path`);
 
-const dbPath = path.resolve(__dirname, `sleeptracker.db`) 
+const dbPath = path.resolve(__dirname, `sleeptracker.db`);
+
+const db = new Database(dbPath, { verbose : console.log});
