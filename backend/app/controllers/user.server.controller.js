4c86287e4fa996a67990e4b9855d8c8414388f0b
IsaacRid <isaac.rideal@gmail.com>
2026-02-15 15:16:22 +0000

    feat: Set up file structure

---
+++ backend/app/controllers/user.server.controller.js
@@ -0,0 +1,27 @@
+const login = (req, res) => {
+
+}
+
+const signUp = (req, res) => {
+
+}
+
+const logout = (req, res) => {
+
+}
+
+const getProfile = (req, res) => {
+
+}
+
+const updateProfile = (req, res) => {
+
+}
+
+module.exports = {
+    login,
+    signUp,
+    logout,
+    getProfile,
+    updateProfile
+}
