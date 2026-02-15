897e4c69e610637d139461105041ef3ba47e5911
IsaacRid <isaac.rideal@gmail.com>
2026-02-15 13:51:14 +0000

    Added all routes

--- backend/app/routes/user.server.routes.js
+++ backend/app/routes/user.server.routes.js
@@ -1,1 +1,14 @@
-const user = require(`../controllers/user.server.controller`)
+const user = require("../controllers/user.server.controller");
+
+module.exports = function (app) {
+    app.route("/login")
+        .post(user.login)
+    app.route("/signup")
+        .post(user.sign_up)
+    app.route("/logout")
+        .post(user.log_out)
+    app.route("/profile/:id")
+        .get(user.get_profile)
+    app.route("/profile")
+        .put(user.update_profile)
+}
