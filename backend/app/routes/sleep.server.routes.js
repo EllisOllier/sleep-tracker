b7ac49f7bfd40452ade65892ad27730b7bde6e41
IsaacRid <isaac.rideal@gmail.com>
2026-02-15 14:33:36 +0000

    Added all routes

--- backend/app/routes/sleep.server.routes.js
+++ backend/app/routes/sleep.server.routes.js
@@ -1,1 +1,11 @@
 const sleep = require("../controllers/sleep.server.controller");
+
+module.exports = function (app) {
+    app.route("/sleep")
+        .post(sleep.addSleepEntry)
+        .get(sleep.getAllSleeps)
+    app.route("/sleep/:id")
+        .get(sleep.getSleepEntry)
+        .delete(sleep.deleteSleep)
+        .put(sleep.editSleep)
+}
