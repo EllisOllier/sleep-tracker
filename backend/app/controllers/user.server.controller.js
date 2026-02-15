c9f95854b5785e637f2fed8892dbcec38032213d
IsaacRid <isaac.rideal@gmail.com>
2026-02-15 20:39:05 +0000

    feat: Added data validation

--- backend/app/controllers/user.server.controller.js
+++ backend/app/controllers/user.server.controller.js
@@ -1,11 +1,29 @@
 const Joi = require("joi");
 
 const login = (req, res) => {
-    const { email, password } = req.body;
+    const loginSchema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(25).pattern(/[A-Z]/, 'uppercase').pattern(/[a-z]/, 'lowercase').pattern(/[^A-Za-z0-9]/, 'special').pattern(/\d/).required()
+    }).unknown(false);
+    const { error } = loginSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error_message: error.details[0].message });
+    }
 }
 
 const signUp = (req, res) => {
+    const signUpSchema = Joi.object({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(25).pattern(/[A-Z]/, 'uppercase').pattern(/[a-z]/, 'lowercase').pattern(/[^A-Za-z0-9]/, 'special').pattern(/\d/).required()
+    }).unknown(false);
+    const { error } = signUpSchema.validate(req.body);
 
+    if (error) {
+        return res.status(400).json({ error_message: error.details[0].message })
+    }
 }
 
 const logout = (req, res) => {
@@ -17,7 +35,18 @@ const getProfile = (req, res) => {
 }
 
 const updateProfile = (req, res) => {
+    const updateProfileSchema = Joi.object({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(25).pattern(/[A-Z]/, 'uppercase').pattern(/[a-z]/, 'lowercase').pattern(/[^A-Za-z0-9]/, 'special').pattern(/\d/).required()
+    }).unknown(false);
 
+    const { error } = updateProfileSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error_message: error.details[0].message });
+    }
 }
 
 module.exports = {
